5f821ccf26fe2365d80d4b863722ef63a3ff3732
IvanS291 <escorpion_sup@hotmail.com>
2018-06-12 11:19:59 -0500

    Ivan Silva

--- hola.cpp
+++ hola.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-<<<<<<< HEAD
-=======
+
+
 /*Hola commit 2*/
 /*Víctor Manuel Santos Martínez*/
 /*Torrez hernandez jose antonio*/
->>>>>>> c67044fe6a22759b6a409061abf1c1cf4f97b9e3
+	/*Ivan Silva Rodriguez*/
+
 using namespace std;
-//Hola commit 2
+
 //Commit Rogelio Uriel Vergara Covarrubias
 main()
 {
