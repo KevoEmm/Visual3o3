a8de37062a343659ba8c9302a7b1acd579bb6f7a
JAtorrez <pepe17199@hotmail.com>
2018-06-20 00:58:22 -0500

    esta version es para el tutorial

--- hola.cpp
+++ hola.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
 
-/*Yaquelin Oropeza Camargo*/
-/*Oscar Alexei Ramiero Mier*/
-/*Hola commit 2*/
-/*Víctor Manuel Santos Martínez*/
-/*Torrez hernandez jose antonio*/
-/*SOFIA ORTIZ*/
-/*ZARATE MENDOZA FERNANDO*/
-	/*Ivan Silva Rodriguez*/
-/*VITE BIBIANO CESARITA*/
-/*Palacios Franco Mario*/
-/*Mario Sevilla Ponce*/
-<<<<<<< HEAD
-/*Leandro Sanchez Ostoa*/
-=======
-/**Soto Dimas*/
-//Sanchez Sanchez Isaac Alejandro
-<<<<<<< HEAD
-//Nelli Suarez Martinez
-// Rivera Cabrera Demetrio Oliver
-=======
->>>>>>> 7fc9c01c6dbae06a10a6b7ea9220201753b35738
->>>>>>> bb49ac2b0326240e600044ed558f4e556107b8ed
 using namespace std;
 
-//Commit Rogelio Uriel Vergara Covarrubias
+
 main()
 {
-	cout<<"Hola mundo"<<endl;
+	cout<<"Hola, esta version del aarchivo, se modifico, para el tutorial, de clonar y actualizar un repositorio en git hub"<<endl;
 }
